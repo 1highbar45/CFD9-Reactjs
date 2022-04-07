f3e3ee00ff8c816aa52d1d5d7acf9c5325f75580
Vu Le <1859049@itec.hcmus.edu.vn>
2022-04-07 17:00:50 +0700

    add combineReducers

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux"
+import { combineReducers, createStore } from "redux"
 import { authReducer } from "./auth"
 
 const counterReducer = (state = { value: 0 }, action) => {
